5f0428c19de6cbe9991dbce0a05027f03022a487
ufukanilozluk <ufukanilozluk@gmail.com>
2023-04-23 19:29:06 +0300

    Initial Commit

---
+++ Jodel-iOS-Challange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jodel-iOS-Challange
+//
+//  Created by Ufuk Anıl Özlük on 23.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Jodel-iOS-ChallangeUITests/Jodel_iOS_ChallangeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Jodel_iOS_ChallangeUITestsLaunchTests.swift
+//  Jodel-iOS-ChallangeUITests
+//
+//  Created by Ufuk Anıl Özlük on 23.04.2023.
+//
+
+import XCTest
+
+final class Jodel_iOS_ChallangeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
