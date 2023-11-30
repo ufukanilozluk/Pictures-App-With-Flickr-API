39ff73f75df39075f060b23fa75b5fbe1dac2fcd
ufukanilozluk <ufukanilozluk@gmail.com>
2023-11-30 22:12:18 +0300

    GalleryCollectionViewCell refactor

--- Jodel-iOS-Challange/UI/GalleryCollectionViewCell.swift
+++ Jodel-iOS-Challange/UI/GalleryCollectionViewCell.swift
@@ -4,9 +4,9 @@ import UIKit
 /// Custom `UICollectionViewCell` for displaying a gallery item.
 class GalleryCollectionViewCell: UICollectionViewCell {
   /// The label for displaying the title of the gallery item.
-  @IBOutlet var title: UILabel!
+  @IBOutlet var titleLabel: UILabel!
   /// The slideshow view for displaying images.
-  @IBOutlet var picture: ImageSlideshow!
+  @IBOutlet var pictureView: ImageSlideshow!
   /// Weak reference to the parent view controller to prevent strong reference cycles.
   weak var parentViewController: UIViewController?
   /// The reuse identifier for this cell.
@@ -19,8 +19,9 @@ class GalleryCollectionViewCell: UICollectionViewCell {
   ///   - indexPath: The index path of the cell.
   ///   - parentVC: The parent view controller.
   func set(data: GalleryData.Photos.Photo, indexPath: IndexPath, parentVC: UIViewController) {
-    title.text = data.title
-    addPics(url: data.photoURL, on: picture)
+    titleLabel.text = data.title
+    addPics(url: data.photoURL, on: pictureView)
+
     parentViewController = parentVC
   }
 
@@ -29,22 +30,65 @@ class GalleryCollectionViewCell: UICollectionViewCell {
   /// - Parameters:
   ///   - url: The URL of the image.
   ///   - view: The `ImageSlideshow` view.
-  func addPics(url: String, on view: ImageSlideshow) {
-    guard let alamofireSource = AlamofireSource(urlString: url) else {
+  private func addPics(url: String, on view: ImageSlideshow) {
+    setupSlideshow(with: url, on: view)
+  }
+
+  /// Setup the slideshow with the given URL.
+  ///
+  /// - Parameters:
+  ///   - url: The URL of the image.
+  ///   - view: The `ImageSlideshow` view to display the image.
+  private func setupSlideshow(with url: String, on view: ImageSlideshow) {
+    guard let alamofireSource = createAlamofireSource(from: url) else {
+      addNoImageView(view)
       return
     }
+
     let alamofireSourceArray: [AlamofireSource] = [alamofireSource]
     view.setImageInputs(alamofireSourceArray)
     view.contentScaleMode = UIView.ContentMode.scaleToFill
+    addTapGesture()
+  }
 
-    // Add a tap gesture recognizer for full-screen viewing.
+  /// Create an AlamofireSource from the given URL.
+  ///
+  /// - Parameter url: The URL of the image.
+  /// - Returns: An AlamofireSource object if the URL is valid, otherwise nil.
+  private func createAlamofireSource(from url: String) -> AlamofireSource? {
+    guard let imageURL = URL(string: url) else {
+      return nil
+    }
+    return AlamofireSource(url: imageURL)
+  }
+
+  /// Add a default noimage view to the given ImageSlideshow.
+  ///
+  /// - Parameter view: The `ImageSlideshow` view to which the noimage view will be added.
+  private func addNoImageView(_ view: ImageSlideshow) {
+    let noImageView = UIImageView(image: UIImage(named: "noimage"))
+    view.addSubview(noImageView)
+    noImageView.translatesAutoresizingMaskIntoConstraints = false
+    noImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    noImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+  }
+
+
+  /// Adds a tap gesture recognizer for full-screen viewing.
+  private func addTapGesture() {
     let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTap))
-    picture.addGestureRecognizer(gestureRecognizer)
+    pictureView.addGestureRecognizer(gestureRecognizer)
   }
 
   /// Action to perform when the slideshow view is tapped.
-  @objc func didTap() {
+  @objc private func didTap() {
     guard let parentViewController = parentViewController else { return }
-    picture.presentFullScreenController(from: parentViewController)
+    pictureView.presentFullScreenController(from: parentViewController)
   }
+
+  /// Deinitialize the object and release any resources.
+  deinit {
+    parentViewController = nil
+  }
+
 }
